4baca8c44406fb9baa5f60c3179dc7ecbf5c1cb8
kristijcarp <kcarpenter@alixpartners.com>
2026-07-09 18:19:53 -0400

    Remove Vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/alix-facilitator-hub/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
-})
