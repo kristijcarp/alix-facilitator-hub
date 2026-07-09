a177a1b3c289b84e584f5186c70bcdd3bc59d2a5
kristijcarp <kcarpenter@alixpartners.com>
2026-07-09 17:53:02 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          900: '#003d7a',
+          800: '#0055b8',
+          700: '#0066cc',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
